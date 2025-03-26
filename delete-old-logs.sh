f138e2d2602eae9fa0a5c50853454a7e200db1a5
saikarthikvarmadatla <saikarthikvarma09@gmail.com>
2025-03-26 13:14:51 -0500

    logs

---
+++ delete-old-logs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SOURCE_DIR=/home/ec2-user/logs
+
+if [ -d $SOURCE_DIR ]
+then 
+    echo "$SOURCE_DIR EXISTS"
+else
+    echo "$SOURCE_DIR doesnt exist"
+    exit 1
+fi
+
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
+echo "Files: $FILES"
+
+while IFS= read -r line
+do
+    echo "deleting line : $line"
+    rm -rf $line
+done <<< $FILES
+
