d58563064a633e736bc415df671d43e9a1bec81f
saikarthikvarmadatla <saikarthikvarma09@gmail.com>
2025-03-26 15:09:22 -0500

    logs

--- disk-usage.sh
+++ disk-usage.sh
@@ -6,7 +6,7 @@ DISK_threshold=5
 while IFS= read -r line
 do
     USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    PARTI=$(echo $line | grep xfs | awk -F " " '{print $1F}')
+    PARTI=$(echo $line | grep xfs | awk -F " " '{print $6F}')
     if [ $USAGE -ge $DISK_threshold ]
     then 
         echo "$PARTI is more than $DISK_threshold,cureent value is $USAGE"
