3e0254217c7feec0ed8ea59648d2731507336e57
saikarthikvarmadatla <saikarthikvarma09@gmail.com>
2025-03-26 14:13:36 -0500

    logs

--- backup.sh
+++ backup.sh
@@ -14,3 +14,24 @@ if [ $# -lt 2 ]
 then 
     USAGE
 fi
+
+if [ ! -d $SOURCE_DIR ]
+then    
+    echo "$SOURCE_DIR doesnt exist..please check.."
+fi
+
+if [ ! -d $DEST_DIR ]
+then    
+    echo "$DEST_DIR doesnt exist..please check.."
+fi
+
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
+
+echo "FILES:: $FILES"
+
+if [ ! -z $FILES ] #-z is for to know true if the files are none and ! makes opposite  so if files are not empty then files are found is displayed
+then 
+    echo "Files are found"
+else 
+    echo "no files older than $DAYS"
+fi
